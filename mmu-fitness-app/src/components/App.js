f123e147036ab113a27bc4e15e04c9b5ca565043
Marionwanyama23 <marionwanyama23@gmail.com>
2024-03-15 15:08:26 +0300

    :art: updated the app js file

--- mmu-fitness-app/src/components/App.js
+++ mmu-fitness-app/src/components/App.js
@@ -1,1 +1,10 @@
+import React from "react";
+import CreateExerciseForm from "./CreateExerciseForm";
 
+ function App() {
+  return <div>  
+    <CreateExerciseForm/>
+  </div>
+ }
+
+export default App;
