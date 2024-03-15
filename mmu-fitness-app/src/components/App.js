29eeacfb4a2e505ad726251ac09b525eacbf316c
Marionwanyama23 <marionwanyama23@gmail.com>
2024-03-15 15:24:31 +0300

    :sparkles: created create routine form component

--- mmu-fitness-app/src/components/App.js
+++ mmu-fitness-app/src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import CreateExerciseForm from "./CreateExerciseForm";
 import Header from "./Header";
+import CreateRoutineForm from "./CreateRoutineForm";
 
  function App() {
   return <div>  
     <CreateExerciseForm/>
     <Header/>
+    <CreateRoutineForm/>
   </div>
  }
 
